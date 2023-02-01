eca8df283ed35e25b215d01b73d1cb490d42ae95
lpy716 <lpy716@163.com>
2023-02-01 17:58:52 +0800

    Site updated: 2023-02-01 17:58:43

---
+++ js/fontsize_larger.js
@@ -0,0 +1,8 @@
+// 加大字体
+function fontsize_larger() {
+    const nowSize = document.documentElement.getAttribute('style')
+    if (nowSize === 'light') {
+        ss
+    new ClipboardJS(".font_larger", { text: function() { return '标题：' + document.title + '\n链接：' + url } });
+    btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
+}
